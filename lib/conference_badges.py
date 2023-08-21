d99a149707d219709d099fcf01887cd05da73d8f
John Carges <johncarges@gmail.com>
2023-08-21 15:41:11 -0400

    passes all tests, finally

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,14 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {index+1}!" for index, name in enumerate(names)]
 
 def printer(names):
-    return None
+    for message in batch_badge_creator(names):
+        print(message)
+    for message in assign_rooms(names):
+        print(message)
